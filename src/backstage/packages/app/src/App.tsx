fcf1749e18e81bff890a5eb29e0f2237b86187d8
saguillen <sa.guillen@uniandes.edu.co>
2026-05-31 23:22:13 -0500

    add notifications plugin to app features

--- src/backstage/packages/app/src/App.tsx
+++ src/backstage/packages/app/src/App.tsx
@@ -1,7 +1,8 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import notificationsPlugin from '@backstage/plugin-notifications/alpha'; 
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [catalogPlugin, navModule, notificationsPlugin,],
 });
