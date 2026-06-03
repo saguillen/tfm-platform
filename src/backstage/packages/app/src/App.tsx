72c22f765f35a4d3d55b9ecc44ee4049cf813d99
saguillen <sa.guillen@uniandes.edu.co>
2026-06-02 23:57:56 -0500

    update Backstage app configuration and Dockerfile for frontend integration

--- src/backstage/packages/app/src/App.tsx
+++ src/backstage/packages/app/src/App.tsx
@@ -1,5 +1,7 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import homePlugin from '@backstage/plugin-home/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import notificationsPlugin from '@backstage/plugin-notifications/alpha';
 import searchPlugin from '@backstage/plugin-search/alpha'; 
 import { navModule } from './modules/nav';
@@ -7,5 +9,12 @@ import { navModule } from './modules/nav';
 
 
 export default createApp({
-  features: [catalogPlugin, navModule, notificationsPlugin, searchPlugin,],
+  features: [
+    homePlugin,
+    catalogPlugin,
+    kubernetesPlugin,
+    navModule,
+    notificationsPlugin,
+    searchPlugin,
+  ],
 });
