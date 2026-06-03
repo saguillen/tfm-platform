db0a61eb6f56c20fd4268169f8e47c48906e4201
saguillen <sa.guillen@uniandes.edu.co>
2026-06-03 09:40:29 -0500

    Revert "add target URL for catalog entities in app configuration files"
    This reverts commit d1c540c0f162b5a1991515689ea5f0ca4fa27cf1.

--- src/backstage/packages/app/src/App.tsx
+++ src/backstage/packages/app/src/App.tsx
@@ -1,7 +1,5 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
-import homePlugin from '@backstage/plugin-home/alpha';
-import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import notificationsPlugin from '@backstage/plugin-notifications/alpha';
 import searchPlugin from '@backstage/plugin-search/alpha'; 
 import { navModule } from './modules/nav';
@@ -9,12 +7,5 @@ import { navModule } from './modules/nav';
 
 
 export default createApp({
-  features: [
-    homePlugin,
-    catalogPlugin,
-    kubernetesPlugin,
-    navModule,
-    notificationsPlugin,
-    searchPlugin,
-  ],
+  features: [catalogPlugin, navModule, notificationsPlugin, searchPlugin,],
 });
